05fb5fe21b312cad4faf231369b5855e11c1dfd0
zarenui <konrolz6337@gmail.com>
2026-04-20 19:00:00 +0300

    Стартовый метод

---
+++ src/main/java/org/example/project_logistic/SmartAssignApplication.java
@@ -0,0 +1,11 @@
+package org.example.project_logistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAssignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAssignApplication.class, args);
+    }
+}
